19fa46f720a204620f764691bcac044a81341ca3
Rodrigo Diaz <r2d2rigo@gmail.com>
2020-12-15 15:44:29 +0000

    Updated file extract test.

--- ValvePak/ValvePak.Test/PackageTest.cs
+++ ValvePak/ValvePak.Test/PackageTest.cs
@@ -9,14 +9,15 @@ using SteamDatabase.ValvePak;
 namespace Tests
 {
     [TestFixture]
-    public class PackageTest
+    public class PackageTest 
     {
         private static readonly string APEX_VPK_DIRECTORY = "D:\\Steam\\steamapps\\common\\Apex Legends\\vpk";
+        private static readonly string APEX_TEST_VPK_FILENAME = "englishclient_frontend.bsp.pak000_dir.vpk";
 
         [Test]
         public void ParseVPK()
         {
-            var path = Path.Combine(APEX_VPK_DIRECTORY, "englishclient_frontend.bsp.pak000_dir.vpk");
+            var path = Path.Combine(APEX_VPK_DIRECTORY, APEX_TEST_VPK_FILENAME);
 
             using var package = new Package();
             package.Read(path);
@@ -42,14 +43,14 @@ namespace Tests
             using var resource = new Package();
             resource.SetFileName("a.vpk");
 
-            using var ms = new MemoryStream(new byte[] { 0x34, 0x12, 0xAA, 0x55, 0x11, 0x11, 0x11, 0x11, 0x22, 0x22, 0x22, 0x22 });
+            using var ms = new MemoryStream(new byte[] { 0x34, 0x12, 0xAA, 0x55, 0x11, 0x11, 0x11, 0x11, 0x22, 0x22, 0x22, 0x22, 0x33, 0x33, 0x33, 0x33 });
             Assert.Throws<InvalidDataException>(() => resource.Read(ms));
         }
 
         [Test]
         public void FindEntryDeep()
         {
-            var path = Path.Combine(APEX_VPK_DIRECTORY, "englishclient_frontend.bsp.pak000_dir.vpk");
+            var path = Path.Combine(APEX_VPK_DIRECTORY, APEX_TEST_VPK_FILENAME);
 
             using var package = new Package();
             package.Read(path);
@@ -82,34 +83,34 @@ namespace Tests
         [Test]
         public void FindEntryRoot()
         {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "steamdb_test_single.vpk");
+            var path = Path.Combine(APEX_VPK_DIRECTORY, APEX_TEST_VPK_FILENAME);
 
             using var package = new Package();
             package.Read(path);
 
-            Assert.AreEqual(0x9C800116, package.FindEntry("kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry(string.Empty, "kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry(string.Empty, "kitten", "jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry(" ", "kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry(" ", "kitten", "jpg")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry(string.Empty, "gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry(string.Empty, "gameinfo", "txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry(" ", "gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry(" ", "gameinfo", "txt")?.CRC32);
 
-            Assert.AreEqual(0x9C800116, package.FindEntry("\\kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry("\\", "kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry("\\", "kitten", "jpg")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("\\gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("\\", "gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("\\", "gameinfo", "txt")?.CRC32);
 
-            Assert.AreEqual(0x9C800116, package.FindEntry("/kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry("/", "kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry("/", "kitten", "jpg")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("/gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("/", "gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("/", "gameinfo", "txt")?.CRC32);
 
-            Assert.AreEqual(0x9C800116, package.FindEntry("\\/kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry("\\/\\", "kitten.jpg")?.CRC32);
-            Assert.AreEqual(0x9C800116, package.FindEntry("\\\\/", "kitten", "jpg")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("\\/gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("\\/\\", "gameinfo.txt")?.CRC32);
+            Assert.AreEqual(0x744A2D89, package.FindEntry("\\\\/", "gameinfo", "txt")?.CRC32);
         }
 
         [Test]
         public void ThrowsNullArgumentInFindEntry()
         {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "steamdb_test_single.vpk");
+            var path = Path.Combine(APEX_VPK_DIRECTORY, APEX_TEST_VPK_FILENAME);
 
             using var package = new Package();
             package.Read(path);
@@ -123,87 +124,9 @@ namespace Tests
         }
 
         [Test]
-        public void FindEntrySpacesAndExtensionless()
-        {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "broken_dir.vpk");
-
-            using var package = new Package();
-            package.Read(path);
-
-            Assert.AreEqual(0x0BA144CC, package.FindEntry("test")?.CRC32);
-            Assert.AreEqual(0x0BA144CC, package.FindEntry("\\/\\", "test")?.CRC32);
-            Assert.AreEqual(0x0BA144CC, package.FindEntry("\\\\/", "test")?.CRC32);
-            Assert.AreEqual(0x0BA144CC, package.FindEntry("\\\\/", "test", " ")?.CRC32);
-            Assert.AreEqual(0x0BA144CC, package.FindEntry(" ", "test")?.CRC32);
-            Assert.AreEqual(0x0BA144CC, package.FindEntry(" ", "test", " ")?.CRC32);
-
-            Assert.AreEqual(0xBF108706, package.FindEntry("folder with space/test")?.CRC32);
-            Assert.AreEqual(0xBF108706, package.FindEntry("folder with space", "test")?.CRC32);
-            Assert.AreEqual(0xBF108706, package.FindEntry("\\folder with space", "test", " ")?.CRC32);
-            Assert.AreEqual(0xBF108706, package.FindEntry("//folder with space//", "test", " ")?.CRC32);
-
-            Assert.AreEqual(0x09321FC0, package.FindEntry("folder with space\\space_extension. txt")?.CRC32);
-            Assert.AreEqual(0x09321FC0, package.FindEntry("/folder with space", "space_extension. txt")?.CRC32);
-            Assert.AreEqual(0x09321FC0, package.FindEntry("folder with space/", "space_extension", " txt")?.CRC32);
-
-            Assert.AreEqual(0x76D91432, package.FindEntry("folder with space/file name with space.txt")?.CRC32);
-
-            Assert.AreEqual(0x15C1490F, package.FindEntry("uppercasefolder/bad_file_forfun.txt")?.CRC32);
-            Assert.AreEqual(0x32CFF012, package.FindEntry("UpperCaseFolder/UpperCaseFile.txt")?.CRC32);
-
-            Assert.IsNull(package.FindEntry("UpperCaseFolder/bad_file_forfun.txt"));
-            Assert.IsNull(package.FindEntry("uppercasefolder/UpperCaseFile.txt"));
-            Assert.IsNull(package.FindEntry("uppercasefolder/bad_file_forfun.TXT"));
-            Assert.IsNull(package.FindEntry("uppercasefolder/bad_file_forfun.txt2"));
-        }
-
-        [Test]
-        public void TestGetFullPath()
-        {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "broken_dir.vpk");
-
-            using var package = new Package();
-            package.Read(path);
-
-            Assert.AreEqual("test", package.FindEntry("test")?.GetFullPath());
-            Assert.AreEqual("folder with space/test", package.FindEntry("folder with space/test")?.GetFullPath());
-            Assert.AreEqual("folder with space/space_extension. txt", package.FindEntry("folder with space\\space_extension. txt")?.GetFullPath());
-            Assert.AreEqual("uppercasefolder/bad_file_forfun.txt", package.FindEntry("uppercasefolder/bad_file_forfun.txt")?.GetFullPath());
-            Assert.AreEqual("UpperCaseFolder/UpperCaseFile.txt", package.FindEntry("UpperCaseFolder/UpperCaseFile.txt")?.GetFullPath());
-
-            Assert.AreEqual("test", package.FindEntry("test")?.GetFileName());
-            Assert.AreEqual("test", package.FindEntry("folder with space/test")?.GetFileName());
-            Assert.AreEqual("space_extension. txt", package.FindEntry("folder with space\\space_extension. txt")?.GetFileName());
-            Assert.AreEqual("bad_file_forfun.txt", package.FindEntry("uppercasefolder/bad_file_forfun.txt")?.GetFileName());
-        }
-
-        [Test]
-        public void TestPackageEntryToString()
-        {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "broken_dir.vpk");
-
-            using var package = new Package();
-            package.Read(path);
-
-            Assert.AreEqual("test crc=0xba144cc metadatasz=0 fnumber=0 ofs=0x00 sz=39", package.FindEntry("test")?.ToString());
-            Assert.AreEqual("folder with space/test crc=0xbf108706 metadatasz=0 fnumber=0 ofs=0x52 sz=41", package.FindEntry("folder with space/test")?.ToString());
-            Assert.AreEqual("folder with space/space_extension. txt crc=0x9321fc0 metadatasz=0 fnumber=0 ofs=0x7b sz=30", package.FindEntry("folder with space\\space_extension. txt")?.ToString());
-            Assert.AreEqual("uppercasefolder/bad_file_forfun.txt crc=0x15c1490f metadatasz=0 fnumber=0 ofs=0xa2 sz=2", package.FindEntry("uppercasefolder/bad_file_forfun.txt")?.ToString());
-            Assert.AreEqual("UpperCaseFolder/UpperCaseFile.txt crc=0x32cff012 metadatasz=0 fnumber=0 ofs=0x27 sz=43", package.FindEntry("UpperCaseFolder/UpperCaseFile.txt")?.ToString());
-        }
-
-        [Test]
-        public void ExtractInlineVPK()
-        {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "steamdb_test_single.vpk");
-
-            TestVPKExtraction(path);
-        }
-
-        [Test]
         public void ExtractDirVPK()
         {
-            var path = Path.Combine(TestContext.CurrentContext.TestDirectory, "Files", "steamdb_test_dir.vpk");
+            var path = Path.Combine(APEX_VPK_DIRECTORY, APEX_TEST_VPK_FILENAME);
 
             TestVPKExtraction(path);
         }
@@ -213,9 +136,9 @@ namespace Tests
             using var package = new Package();
             package.Read(path);
 
-            Assert.AreEqual(2, package.Entries.Count);
-            Assert.Contains("jpg", package.Entries.Keys);
-            Assert.Contains("proto", package.Entries.Keys);
+            Assert.AreEqual(11, package.Entries.Count);
+            Assert.Contains("txt", package.Entries.Keys);
+            Assert.Contains("cfg", package.Entries.Keys);
 
             var flatEntries = new Dictionary<string, PackageEntry>();
 
@@ -229,23 +152,21 @@ namespace Tests
                     {
                         Assert.AreEqual(a.Key, b.TypeName);
 
-                        flatEntries.Add(b.FileName, b);
+                        flatEntries.Add(b.GetFullPath(), b);
 
                         package.ReadEntry(b, out var entry);
 
-                        data.Add(b.FileName + '.' + b.TypeName, BitConverter.ToString(sha1.ComputeHash(entry)).Replace("-", string.Empty));
+                        data.Add(b.GetFullPath(), BitConverter.ToString(sha1.ComputeHash(entry)).Replace("-", string.Empty));
                     }
                 }
 
-                Assert.AreEqual(3, data.Count);
-                Assert.AreEqual("E0D865F19F0A4A7EA3753FBFCFC624EE8B46928A", data["kitten.jpg"]);
-                Assert.AreEqual("2EFFCB09BE81E8BEE88CB7BA8C18E87D3E1168DB", data["steammessages_base.proto"]);
-                Assert.AreEqual("22741F66442A4DC880725D2CC019E6C9202FD70C", data["steammessages_clientserver.proto"]);
+                Assert.AreNotEqual(0, data.Count);
+                Assert.AreEqual("A9FF3616D6D58C78579D1A49CDB469A22D068D37", data["gameinfo.txt"]);
+                Assert.AreEqual("2EF43AAF78B644702990D43F0F72ADAB8E644396", data["resource/notosansjp-regular.vfont"]);
             }
 
-            Assert.AreEqual(flatEntries["kitten"].TotalLength, 16361);
-            Assert.AreEqual(flatEntries["steammessages_base"].TotalLength, 2563);
-            Assert.AreEqual(flatEntries["steammessages_clientserver"].TotalLength, 39177);
+            Assert.AreEqual(flatEntries["gameinfo.txt"].TotalLength, 1498);
+            Assert.AreEqual(flatEntries["resource/notosansjp-regular.vfont"].TotalLength, 4479600);
         }
     }
 }
